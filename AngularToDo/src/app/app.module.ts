860de698a1d24582ef6f72c83698c32016672483
Chris Wiseman <wisemanchris92@gmail.com>
2022-03-31 12:56:00 -0500

    Added todo list

--- AngularToDo/src/app/app.module.ts
+++ AngularToDo/src/app/app.module.ts
@@ -10,6 +10,12 @@ import { TodoComponent } from './todo/todo.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,10 +26,15 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularToDo/src/app/todo/todo.component.ts
+++ AngularToDo/src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { ITask } from '../model/task';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  todoForm !: FormGroup; //! tells angular that this will be defined later on
+  tasks : ITask [] = [];
+  inProgress : ITask [] = [];
+  done: ITask [] = [];
+  
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      item : ['', Validators.required]
+    })
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }
